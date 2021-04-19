9a7b89865ddfc81e29c59d8ec7de653560b4ebab
whwjdan <whwjdan@naver.com>
2021-04-19 13:10:36 +0900

    refactor: Controller -> Repository

--- eatgo-api/src/main/java/com/jojeongmu/springeat/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/com/jojeongmu/springeat/interfaces/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.jojeongmu.springeat.interfaces;
 
 import com.jojeongmu.springeat.domain.Restaurant;
+import com.jojeongmu.springeat.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,31 +11,27 @@ import java.util.List;
 
 @RestController
 public class RestaurantController {
-
+    
+    private RestaurantRepository repository = new RestaurantRepository();
+    
     @GetMapping("/restaurant")
     public List<Restaurant> list(){
+/*
         List<Restaurant> restaurants = new ArrayList<>();
 
-        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+*/
 
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurants = repository.findById(id);
 
-        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-
-
-        return restaurant;
+        return restaurants;
     }
 }
