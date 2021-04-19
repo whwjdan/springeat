6cb5af27015ae4a1218e0cc50d37dc0b7079f4b3
whwjdan <whwjdan@naver.com>
2021-04-19 11:19:58 +0900

    first commit

---
+++ src/test/java/com/jojeongmu/springeat/SpringeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojeongmu.springeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
