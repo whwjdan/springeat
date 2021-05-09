c8fc1716fc316523cb8e180c27e21a22325fc645
whwjdan <whwjdan@naver.com>
2021-05-09 15:30:11 +0900

    bulkUpdate Test

--- eatgo-api/src/main/java/com/jojeongmu/springeat/application/MenuItemService.java
+++ eatgo-api/src/main/java/com/jojeongmu/springeat/application/MenuItemService.java
@@ -1,6 +1,9 @@
 package com.jojeongmu.springeat.application;
 
 import com.jojeongmu.springeat.domain.MenuItem;
+import com.jojeongmu.springeat.domain.MenuItemRepository;
+import com.jojeongmu.springeat.domain.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,7 +11,20 @@ import java.util.List;
 @Service
 public class MenuItemService {
 
+    private MenuItemRepository menuItemRepository;
+
+    @Autowired
+    public MenuItemService(MenuItemRepository menuItemRepository){
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    public MenuItemService() {
+
+    }
+
     public void bulkUpdate(Long restaurantId, List<MenuItem> menuItems){
-        // TODO: bulk update
+        for(MenuItem menuItem : menuItems){
+            menuItemRepository.save(menuItem);
+        }
     }
 }
